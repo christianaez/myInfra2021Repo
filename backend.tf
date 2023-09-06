ffadecdaa4c95b3a3817c6efef89942e7a81ae8c
christianaez <88499471+christianaez@users.noreply.github.com>
2023-09-06 16:01:11 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-aws-tf-state-bucket"
-    key = "main"
-    region = "us-east-1"
+    bucket = "my-amazon-tf-state-bucket"
+    key = "master"
+    region = "ca-central-1"
     dynamodb_table = "my-dynamo-db-table"
   }
 }
